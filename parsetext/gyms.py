f6d9cff982ca8977eaa785fde914fb054660286f
Landen Bailey <54867037+landiinii@users.noreply.github.com>
2024-11-11 09:40:09 -0700

    Add all new gyms

--- parsetext/gyms.py
+++ parsetext/gyms.py
@@ -6,13 +6,15 @@ con = duckdb.connect("workouts.db")
 gym_map = {
     "Valhalla": {"AF": {}},
     "EOS": {"Orem": {}},
-    "Vasa": {"AF": {}, "Saratoga": {}, "Herriman": {}, "Provo": {}, "Lindon": {}},
+    "Vasa": {"AF": {}, "Saratoga": {}, "Herriman": {}, "Provo": {}, "Lindon": {}, "University": {}},
     "Rec_Center": {"Provo_Fieldhouse": {}, "Snow_College": {}},
     "Vacation": {
         "Jungle_Gym_Tulum_Beach": {},
         "Jungle_Gym_Tulum_City": {},
         "Ukiyo_Hawaii": {},
         "Sonnys_USVI": {},
+        "Iron_Legacy_Barbell": {},
+        "Alphaland": {},
     },
     "Competition": {
         "Bishop_Barbell": {},
@@ -21,10 +23,14 @@ gym_map = {
         "Highland_Fling": {},
         "Brazen_Gathering": {},
         "Sinister_Games": {},
+        "Stones_of_Strength": {},
+        "Viking_Power": {},
+        "Bailey_Family_Reunion": {},
+        "Welsh_Days": {},
     },
-    "Hotel_Gym": {"Pune": {}, "Hawaii": {}, "Las_Vegas_Flamingo": {}},
+    "Hotel_Gym": {"Pune": {}, "Hawaii": {}, "Las_Vegas_Flamingo": {}, "Baner": {}},
     "Home_Gym": {"Bryans": {}},
-    "Work_Gym": {"Innovation_Point": {}},
+    "Work_Gym": {"Innovation_Point": {}, "Redo_Office": {}},
 }
 
 for gym in gym_map:
